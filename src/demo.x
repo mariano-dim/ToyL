7484270982d80aaf3a25307da5edcfceaa053549
mariano.dim <mariano.dim@gmail.com>
2019-10-28 00:15:27 -0300

    regulo stage

---
+++ src/demo.x
@@ -0,0 +1,20 @@
+begin
+var x : int;
+x := 12 + 3;
+# Comentario x = 15
+var t : int;
+t := 25;
+if (t = 25)
+begin
+    x := x * t;
+end
+else
+begin
+    x := 1 + t;
+    if (10  < 1)
+    begin
+        x := 5;
+    end
+end
+print(x);
+end
---
+++ src/initializer.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#
+#  Proyecto X
+#  xinitializer.py
+#
+#  Creado el 29/09/2019
+#  Mariano Andres Di Maggio <mariano.dim@gmail.com>
+#
+
+import os
+import sys
+from builtins import print
+#import xinterpreter
+
+from lexer import Lexer
+from parser import Parser
+
+
+def main():
+    content = ""  # Esta variable mandendra el contenido del archivo de entrada leido
+    path = os.getcwd()
+
+    print('|-|-|-|-|-|-|-|-|-|-|-|-|-|-  Starting Interpreter  |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|- \n')
+    print("Leyendo archivo...")
+
+    # Verifico que el usuario haya ingresado el nombre del archivo como parametro
+    try:
+        fileName = sys.argv[1]
+    except:
+        print(
+            "[ERROR] Se esperaba el nombre de archivo, como parametro para iniciar procesamiento, por ejemplo: 'demo.x'")
+        return
+
+    # Chequeo si la extension del archivo es correcta
+    if fileName[len(fileName) - 2:len(fileName)] != ".x":
+        print("[ERROR] Extension de archivo no reconocida, asegurese que la extension del archivo sea '.x'")
+        return
+
+    # Abre el archivo de entrada (en modo lectura) y lo graba en la variable 'content'
+    try:
+        with open(path + "/" + fileName, "r") as sourceCodeFile:
+            content = sourceCodeFile.read()
+    except:
+        print('[ERROR] No se puede encontrar el archivo "' + fileName + '"')
+
+    # Create lexer
+    lexer = Lexer().get_lexer()
+    tokens = lexer.lex(content)
+    # Create parser
+    pg = Parser()
+    pg.parse()
+    parser = pg.get_parser()
+    parser.parse(tokens).eval()
+    names = pg.get_names().getAllSymbols()
+    for sym in names.keys():
+        print('Symbols Table name ' + str(sym) + ' = ' + str(pg.get_names().getSymbol(sym)))
+
+
+
+
+if __name__ == '__main__':
+    main()
